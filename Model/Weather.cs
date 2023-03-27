605f22c45b3e083f9fb86d0fac4fcc4cb4fe986d
tavlal <98895331+tavlal@users.noreply.github.com>
2023-03-27 14:58:19 +0700

    Добавьте файлы проекта.

---
+++ Model/Weather.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Model
+{
+    public class Weather : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        public coord _coord;
+        public main _main;
+        public string _name;
+        public DayOfWeek _day;
+        public coord coord
+        {
+            get { return _coord; }
+            set
+            {
+                _coord = value; OnPropertyChanged();
+            }
+        }
+        public main main
+        {
+            get { return _main; }
+            set { _main = value; OnPropertyChanged(); }
+        }
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+        public DayOfWeek day
+        {
+            get { return _day; }
+            set { _day = value; OnPropertyChanged(); }
+        }
+    }
+
+
+
+    public class coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+    public class main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double pressure { get; set; }
+    }
+}
---
+++ ViewModel/VM.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Model;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ViewModel
+{
+    public class VM : INotifyPropertyChanged
+    {
+        private readonly string apiKey = File.ReadAllText("apiKey.txt");
+        private Weather newWeather;
+        public Weather NewWeather
+        {
+            get { return newWeather; }
+            set { newWeather = value; OnPropertyChanged(); }
+        }
+        public VM() 
+        {
+            newWeather = new Weather();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        private ICommand getJsonDataLocationCommand;
+        public ICommand GetJsonDataLocationCommand => getJsonDataLocationCommand ?? (getJsonDataLocationCommand = new RelayCommand(GetJsonDataLocation));
+        public async void GetJsonDataLocation()
+        {
+            using (FileStream file = new FileStream("weather_on_locate.json", FileMode.Create))
+            {
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage httpResponse = await httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat=56.0090968&lon=92.8725147&units=metric&appid={apiKey}");
+                string value = await httpResponse.Content.ReadAsStringAsync();
+                var buffer = Encoding.UTF8.GetBytes(value);
+                file.Write(buffer, 0, buffer.Length);
+            }
+            NewWeather = JsonConvert.DeserializeObject<Weather>(File.ReadAllText("weather_on_locate.json"));
+            NewWeather.day = DateTime.Now.DayOfWeek;
+        }
+    }
+}
